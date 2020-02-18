e1a83cd25010438079abaec64fb6a2cc27fd89ef
Omar Alvarez <oalvarez86@gmail.com>
2020-02-17 22:09:44 -0500

    Web Service Consulta Carvajal

---
+++ wsConsultaFepe/PasswordDigestMessageInspector.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Xml;
+
+namespace wsConsultaFepe
+{
+    public class PasswordDigestMessageInspector : IClientMessageInspector
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public PasswordDigestMessageInspector(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        #region IClientMessageInspector Members
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            return;
+        }
+
+        public object BeforeSendRequest(ref Message request, System.ServiceModel.IClientChannel channel)
+        {
+            DateTime created = DateTime.Now;
+            string createdString = created.ToString("yyyy-MM-ddTHH:mm:ss.fff-05:00");
+
+            var envelope = new XmlDocument();
+
+            XmlNode securityNode = envelope.CreateNode(XmlNodeType.Element, "wsse:Security", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+
+            XmlNode usernameTokenNode = envelope.CreateNode(XmlNodeType.Element, "wsse:UsernameToken", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            XmlElement userElement = usernameTokenNode as XmlElement;
+            userElement.SetAttribute("xmlns:wsu", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+
+            XmlNode userNameNode = envelope.CreateNode(XmlNodeType.Element, "wsse:Username", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            userNameNode.InnerXml = Username;
+
+            XmlNode pwdNode = envelope.CreateNode(XmlNodeType.Element, "wsse:Password", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            XmlElement pwdElement = pwdNode as XmlElement;
+            pwdElement.SetAttribute("Type", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText");
+            pwdNode.InnerXml = SHA256Hash(Password).ToLower();
+
+            XmlNode nonceNode = envelope.CreateNode(XmlNodeType.Element, "wsse:Nonce", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            XmlElement nonceElement = nonceNode as XmlElement;
+            nonceElement.SetAttribute("EncodingType", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary");
+            nonceNode.InnerXml = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(DateTime.Now.ToBinary().ToString()));
+
+            XmlNode createNode = envelope.CreateNode(XmlNodeType.Element, "wsu", "Created", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+            XmlElement createElement = createNode as XmlElement;
+            createNode.InnerXml = createdString;
+
+            usernameTokenNode.AppendChild(userNameNode);
+            usernameTokenNode.AppendChild(pwdNode);
+            usernameTokenNode.AppendChild(nonceNode);
+            usernameTokenNode.AppendChild(createNode);
+
+            //securityNode.AppendChild(usernameTokenNode);
+
+            var securityHeader = MessageHeader.CreateHeader("Security", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", usernameTokenNode);
+
+            request.Headers.Add(securityHeader);
+
+            return Convert.DBNull;
+        }
+
+        #endregion
+
+        private static byte[] buildBytes(string nonce, string createdString, string basedPassword)
+        {
+            byte[] nonceBytes = System.Convert.FromBase64String(nonce);
+            byte[] time = Encoding.UTF8.GetBytes(createdString);
+            byte[] pwd = Encoding.UTF8.GetBytes(basedPassword);
+
+            byte[] operand = new byte[nonceBytes.Length + time.Length + pwd.Length];
+            Array.Copy(nonceBytes, operand, nonceBytes.Length);
+            Array.Copy(time, 0, operand, nonceBytes.Length, time.Length);
+            Array.Copy(pwd, 0, operand, nonceBytes.Length + time.Length, pwd.Length);
+
+            return operand;
+        }
+
+        public static byte[] SHAOneHash(byte[] data)
+        {
+            using (SHA256 sha1 = new SHA256Managed())
+            {
+                var hash = sha1.ComputeHash(data);
+                string da = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes("FepePri20$"))).Replace("-", "");
+                return hash;
+            }
+        }
+
+        public static string SHA256Hash(string pass)
+        {
+            using (SHA256 sha1 = new SHA256Managed())
+            {
+                string da = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(pass))).Replace("-", "");
+                return da;
+            }
+        }
+    }
+}
---
+++ wsConsultaFepe/wsConsultaFepe.asmx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Web.Services;
+using FacturacionElectronicaEntidades;
+using CarvajalSrv;
+using System.ServiceModel;
+using System.Web.Configuration;
+
+namespace wsConsultaFepe
+{
+    /// <summary>
+    /// Descripción breve de wsConsultaFepe
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class wsConsultaFepe : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public EstadoInfo ConsultarEstadoDocumento(string sCompania, string sIdTipoDoc, string sSerie,
+                                                   string sNroDocumento)
+        {
+            try
+            {
+
+                var sUsuarioWs = "";
+                var sPwdWs = "";
+                String sTipoDocCrv = "";
+
+                //Usuarios de Web Service
+                switch (sCompania)
+                {
+                    //PRIMAX
+                    case "20554545743":
+                        sUsuarioWs = WebConfigurationManager.AppSettings["UsuarioPrimax"];
+                        sPwdWs = WebConfigurationManager.AppSettings["PwdPrimax"];
+                        break;
+                    //COESTI
+                    case "20127765279":
+                        sUsuarioWs = WebConfigurationManager.AppSettings["UsuarioCoesti"];
+                        sPwdWs = WebConfigurationManager.AppSettings["PwdCoesti"];
+                        break;
+                    //JOVEME
+                    case "20512767011":
+                        sUsuarioWs = WebConfigurationManager.AppSettings["UsuarioJoveme"];
+                        sPwdWs = WebConfigurationManager.AppSettings["PwdJoveme"];
+                        break;
+                    //CODESA
+                    case "20602544002":
+                        sUsuarioWs = WebConfigurationManager.AppSettings["UsuarioCodesa"];
+                        sPwdWs = WebConfigurationManager.AppSettings["PwdCodesa"];
+                        break;
+                }
+
+                //Tipo de Documento
+                if (sIdTipoDoc.Equals("01"))
+                {
+                    sTipoDocCrv = "FA";
+                }
+                else
+                {
+                    if (sIdTipoDoc.Equals("03"))
+                    {
+                        sTipoDocCrv = "BO";
+                    }
+                    else
+                    {
+                        if (sIdTipoDoc.Equals("07"))
+                        {
+                            sTipoDocCrv = "NC";
+                        }
+                        else
+                        {
+                            if (sIdTipoDoc.Equals("08"))
+                            {
+                                sTipoDocCrv = "ND";
+                            }
+                            else
+                            {
+                                if (sIdTipoDoc.Equals("20"))
+                                {
+                                    sTipoDocCrv = "20";
+                                }
+                                else
+                                {
+                                    if (sIdTipoDoc.Equals("40"))
+                                    {
+                                        sTipoDocCrv = "40";
+                                    }
+                                }
+                            }
+                        }
+                                    
+                    }
+                }
+
+                invoiceServiceClient _proxyConsultas = new invoiceServiceClient();
+                var behavior = new PasswordDigestBehavior(sUsuarioWs, sPwdWs);
+                _proxyConsultas.Endpoint.EndpointBehaviors.Add(behavior);
+
+                var res = _proxyConsultas.DocumentStatusByNumber(new DocumentStatusByNumberRequest()
+                {
+                    companyId = sCompania,
+                    accountId = "ACCOUNT000",
+                    documentType = sTipoDocCrv,
+                    documentPrefix = sSerie,
+                    documentNumber = sNroDocumento
+                });
+
+                var oEstadoDocumento = new EstadoInfo();
+
+
+                if (res != null)
+                {
+
+                    switch (res.legalStatus)
+                    {
+                        case "ACCEPTED":
+                            oEstadoDocumento.IdEstado = "ACC";
+                            break;
+                        case "ACCEPTED_WITH_OBSERVATIONS":
+                            oEstadoDocumento.IdEstado = "ACCWO";
+                            break;
+                        case "INCIDENT":
+                            oEstadoDocumento.IdEstado = "INC";
+                            break;
+                        case "EXCEPTION":
+                            oEstadoDocumento.IdEstado = "EXC";
+                            break;
+
+                    }
+
+                    //oEstadoDocumento.IdEstado = res.legalStatus;
+                    oEstadoDocumento.Descripcion = res.governmentResponseDescription;
+                    return oEstadoDocumento;
+                }
+                oEstadoDocumento.IdEstado = "NF";
+                oEstadoDocumento.Descripcion = "Documento no encontrado en Carvajal";
+
+                return oEstadoDocumento;
+
+
+
+            }
+            catch (FaultException<InvoiceServiceFault> ex)
+            {
+                return new EstadoInfo { IdEstado = "ER", Descripcion = ex.Detail.statusCode.ToString() + " - " + ex.Detail.errorMessage.ToString() };
+            }
+            catch (Exception ex)
+            {
+                return new EstadoInfo { IdEstado = "ER", Descripcion = ex.Message };
+            }
+
+
+
+
+        }
+    }
+}
